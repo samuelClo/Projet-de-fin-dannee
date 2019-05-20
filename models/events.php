d81b1593e1febda0966078dadfea2a4b1f2582ca
sam <cloarec.samuel@hotmail.fr>
2019-05-20 23:35:08 +0200

    avancement sur la page event

--- models/events.php
+++ models/events.php
@@ -1,9 +1,30 @@
 <?php
 
+function getEventlist($time = NULL){
 
 
-function getEventlist(){
+    if ($time){
+        
+    }
     $db = dbConnect();
+    $res = new stdClass();
+
     $selectedEventslist = $db->query('SELECT title,description,content,title_picture,secondary_picture FROM events WHERE is_published = 1 AND posted_at  <= NOW()');
-    return $selectedEventslist->fetchAll();
+
+    $eventList = $selectedEventslist->fetchAll()
+
+
+    for ( $i = 0 ; $i < sizeof($eventList) ; $i++ ){
+
+        $array[$i] = [
+            "title" => $eventList[$i]['title'],
+            "description" => $eventList[$i]['description'],
+            "content" => $eventList[$i]['content'],
+            "title_picture" =>$eventList[$i]['title_picture'],
+            "secondary_picture" => $eventList[$i]['secondary_picture'],
+        ];
+
+    }
+
+    return $res->arrayAllEvents = $array;
 }
